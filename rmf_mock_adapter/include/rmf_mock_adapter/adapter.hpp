855cd936dc791b886494dd0da12ae39cfd9933c1
Michael X. Grey <grey@openrobotics.org>
2020-05-15 15:08:29 +0800

    Create a library for the mock API

---
+++ rmf_mock_adapter/include/rmf_mock_adapter/adapter.hpp
@@ -0,0 +1,165 @@
+/*
+ * Copyright (C) 2020 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
+#ifndef RMF_MOCK_ADAPTER__ADAPTER_HPP
+#define RMF_MOCK_ADAPTER__ADAPTER_HPP
+
+#include <rmf_traffic/agv/Planner.hpp>
+
+namespace rmf_mock_adapter {
+
+//==============================================================================
+/// Implement this class to receive robot commands from RMF
+class RobotCommandHandle
+{
+public:
+
+  /// Have the robot follow a new path. If it was already following a path, then
+  /// it should immediately switch over to this one.
+  ///
+  /// \param[in] waypoints
+  ///   The sequence of waypoints to follow. When the robot arrives at a
+  ///   waypoint in this sequence, it should wait at that waypoint until the
+  ///   clock reaches the time() field of the waypoint. This is important
+  ///   because the waypoint timing is used to avoid traffic conflicts with
+  ///   other vehicles.
+  ///
+  /// \param[in] path_finished_callback
+  ///   Trigger this callback when the robot is done following the new path.
+  virtual void follow_new_path(
+      const std::vector<rmf_traffic::agv::Plan::Waypoint>& waypoints,
+      std::function<void()> path_finished_callback) = 0;
+
+  /// Have the robot begin a pre-defined docking procedure. Implement this
+  /// function as a no-op if your robots do not perform docking procedures.
+  ///
+  /// \param[in] dock_name
+  ///   The predefined name of the docking procedure to use.
+  ///
+  /// \param[in] docking_finished_callback
+  ///   Trigger this callback when the docking is finished.
+  virtual void dock(
+      const std::string& dock_name,
+      std::function<void()> docking_finished_callback) = 0;
+
+};
+
+//==============================================================================
+/// You will be given an instance of this class every time you add a new robot
+/// to your fleet. Use that instance to send updates to RoMi-H about your
+/// robot's state.
+///
+/// To remove the available of this robot, simply allow the instance to expire.
+class RobotUpdateHandle
+{
+public:
+
+  /// Tell the RMF schedule that the robot has been delayed.
+  void add_delay(rmf_traffic::Duration duration);
+
+  /// Tell the RMF schedule that the robot was interrupted and needs a new plan.
+  void interrupted();
+
+  /// Update the current position of the robot by specifying the waypoint that
+  /// the robot is on and its orientation.
+  void update_position(
+      std::size_t waypoint,
+      double orientation);
+
+  /// Update the current position of the robot by specifying the x, y, yaw
+  /// position of the robot and one or more lanes that the robot is occupying.
+  ///
+  /// If no lane is specified, then the adapter will assume that the robot has
+  /// exited the navigation graph.
+  void update_position(const Eigen::Vector3d& position,
+      const std::vector<std::size_t>& lanes);
+
+  class Implementation;
+private:
+  rmf_utils::unique_impl_ptr<Implementation> _pimpl;
+};
+
+//==============================================================================
+/// Each fleet will be given an instance of this class to update RoMi-H about
+/// its available robots.
+class FleetUpdateHandle
+{
+public:
+
+  /// Add a new robot, specifying what waypoint it is starting on.
+  std::shared_ptr<RobotUpdateHandle> add_robot(
+      std::shared_ptr<RobotCommandHandle> handle,
+      const std::string& name,
+      const rmf_traffic::Profile& profile,
+      std::size_t initial_waypoint,
+      double orientation);
+
+  /// Add a new robot, specifying what its current position is. This should only
+  /// be used if it is not starting on any waypoint. Use the other signature of
+  /// this function if the robot starts on a waypoint.
+  std::shared_ptr<RobotUpdateHandle> add_robot(
+      std::shared_ptr<RobotCommandHandle> handle,
+      const std::string& name,
+      const rmf_traffic::Profile& profile,
+      const Eigen::Vector3d& initial_position,
+      const std::vector<std::size_t>& initial_lanes);
+
+  class Implementation;
+private:
+  FleetUpdateHandle();
+  rmf_utils::unique_impl_ptr<Implementation> _pimpl;
+};
+
+//==============================================================================
+/// This class mocks up a scenario for testing purposes.
+class TestScenario
+{
+public:
+
+  /// Add a fleet to the test scenario
+  ///
+  /// \param[in] fleet_name
+  ///   The name of the fleet that is being adapted
+  ///
+  /// \param[in] nav_graph
+  ///   The AGV navigation graph information for this fleet
+  FleetUpdateHandle add_fleet(
+      std::string fleet_name,
+      rmf_traffic::agv::Graph nav_graph,
+      rmf_traffic::agv::VehicleTraits traits);
+
+  /// Specify a condition for the test
+  struct Condition
+  {
+    std::shared_ptr<RobotUpdateHandle> robot;
+    rmf_traffic::agv::Plan::Goal goal;
+  };
+
+  /// Initiate a test with the given conditions.
+  void test(std::vector<Condition> conditions);
+
+  /// Constructor
+  TestScenario();
+
+  class Implementation;
+private:
+  rmf_utils::unique_impl_ptr<Implementation> _pimpl;
+};
+
+} // namespace rmf_mock_adapter
+
+#endif // RMF_MOCK_ADAPTER__ADAPTER_HPP
---
+++ rmf_mock_adapter/src/adapter.cpp
@@ -0,0 +1,308 @@
+/*
+ * Copyright (C) 2020 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
+#include <rmf_mock_adapter/adapter.hpp>
+
+#include <rmf_traffic/schedule/Database.hpp>
+
+#include <iostream>
+
+namespace rmf_mock_adapter {
+
+//==============================================================================
+class RobotUpdateHandle::Implementation
+{
+public:
+
+  rmf_traffic::schedule::Participant participant;
+  std::shared_ptr<rmf_traffic::agv::Planner> planner;
+  rmf_traffic::agv::Planner::Options options;
+  std::shared_ptr<RobotCommandHandle> command;
+  std::vector<rmf_traffic::agv::Plan::Start> position;
+
+  rmf_utils::optional<rmf_traffic::agv::Plan::Goal> goal = rmf_utils::nullopt;
+
+  void send_path_command()
+  {
+    if (!goal)
+    {
+      std::cerr << "ERROR: Trying to issue a path command before a goal is set!"
+                << std::endl;
+      return;
+    }
+
+    auto result = planner->plan(position, *goal);
+    if (!result)
+    {
+      std::cerr << "Unable to find a feasible plan.\nStart conditions:";
+      for (const auto& s : position)
+      {
+        std::cout << " -- initial waypoint: " << s.waypoint();
+        if (s.location())
+        {
+          std::cout << " | initial location: (" << s.location()->transpose()
+                    << ")";
+        }
+
+        if (s.lane())
+        {
+          std::cout << " | initial lane: " << *s.lane() << std::endl;
+        }
+      }
+      return;
+    }
+
+    participant.set(result->get_itinerary());
+    std::cout << "Issuing new path command to ["
+              << participant.description().name() << "]" << std::endl;
+    command->follow_new_path(
+          result->get_waypoints(),
+          [&]()
+    {
+      std::cout << "[" << participant.description().name() << "] has reported "
+                << "finishing!" << std::endl;
+    });
+  }
+
+  static std::shared_ptr<RobotUpdateHandle> make_shared(
+      const rmf_traffic::schedule::Viewer& viewer,
+      rmf_traffic::schedule::Participant participant,
+      std::shared_ptr<rmf_traffic::agv::Planner> planner,
+      std::shared_ptr<RobotCommandHandle> command,
+      std::vector<rmf_traffic::agv::Plan::Start> position)
+  {
+    auto handle = std::shared_ptr<RobotUpdateHandle>(new RobotUpdateHandle);
+    auto options = rmf_traffic::agv::Planner::Options(
+          rmf_utils::make_clone<rmf_traffic::agv::ScheduleRouteValidator>(
+            viewer, participant.id(), participant.description().profile()));
+
+    handle->_pimpl = rmf_utils::make_unique_impl<Implementation>(
+          Implementation{
+            std::move(participant),
+            std::move(planner),
+            std::move(options),
+            std::move(command),
+            std::move(position)
+          });
+
+    return handle;
+  }
+
+  static void set_goal(
+      RobotUpdateHandle& handle,
+      rmf_traffic::agv::Plan::Goal goal)
+  {
+    handle._pimpl->goal = std::move(goal);
+    handle._pimpl->send_path_command();
+  }
+};
+
+//==============================================================================
+std::vector<rmf_traffic::agv::Plan::Start> make_starts(
+    std::size_t initial_waypoint,
+    double orientation)
+{
+  const auto now = std::chrono::steady_clock::now();
+  return {rmf_traffic::agv::Plan::Start(now, initial_waypoint, orientation)};
+}
+
+//==============================================================================
+std::vector<rmf_traffic::agv::Plan::Start> make_starts(
+    const rmf_traffic::agv::Graph& nav_graph,
+    const Eigen::Vector3d& initial_position,
+    const std::vector<std::size_t>& initial_lanes)
+{
+  const auto now = std::chrono::steady_clock::now();
+  std::vector<rmf_traffic::agv::Plan::Start> starts;
+  for (const std::size_t l : initial_lanes)
+  {
+    const auto wp = nav_graph.get_lane(l).exit().waypoint_index();
+
+    starts.emplace_back(
+        now, wp, initial_position[2],
+        Eigen::Vector2d(initial_position.block<2,1>(0,0)), l);
+  }
+
+  return starts;
+}
+
+//==============================================================================
+void RobotUpdateHandle::add_delay(rmf_traffic::Duration duration)
+{
+  _pimpl->participant.delay(std::chrono::steady_clock::now(), duration);
+}
+
+//==============================================================================
+void RobotUpdateHandle::interrupted()
+{
+  std::cout << "[" << _pimpl->participant.description().name()
+            << "] was interrupted! We will send a new plan." << std::endl;
+
+  _pimpl->send_path_command();
+}
+
+//==============================================================================
+void RobotUpdateHandle::update_position(
+    std::size_t waypoint,
+    double orientation)
+{
+  _pimpl->position = make_starts(waypoint, orientation);
+}
+
+//==============================================================================
+void RobotUpdateHandle::update_position(
+    const Eigen::Vector3d& position,
+    const std::vector<std::size_t>& lanes)
+{
+  _pimpl->position = make_starts(
+        _pimpl->planner->get_configuration().graph(), position, lanes);
+}
+
+//==============================================================================
+class FleetUpdateHandle::Implementation
+{
+public:
+
+  std::shared_ptr<rmf_traffic::schedule::Database> database;
+  std::string fleet_name;
+  std::shared_ptr<rmf_traffic::agv::Planner> planner;
+
+  static FleetUpdateHandle make(
+      std::shared_ptr<rmf_traffic::schedule::Database> database,
+      std::string fleet_name,
+      rmf_traffic::agv::Graph nav_graph,
+      rmf_traffic::agv::VehicleTraits traits)
+  {
+    auto planner = std::make_shared<rmf_traffic::agv::Planner>(
+          rmf_traffic::agv::Planner::Configuration(
+            nav_graph, traits),
+          rmf_traffic::agv::Planner::Options(nullptr));
+
+    FleetUpdateHandle handle;
+    handle._pimpl = rmf_utils::make_unique_impl<Implementation>(
+          Implementation{
+            std::move(database),
+            std::move(fleet_name),
+            std::move(planner)
+          });
+
+    return handle;
+  }
+
+  std::shared_ptr<RobotUpdateHandle> add_robot(
+      std::shared_ptr<RobotCommandHandle> command,
+      const std::string& name,
+      const rmf_traffic::Profile& profile,
+      std::vector<rmf_traffic::agv::Plan::Start> position)
+  {
+    auto description = rmf_traffic::schedule::ParticipantDescription(
+          name, fleet_name,
+          rmf_traffic::schedule::ParticipantDescription::Rx::Responsive,
+          profile);
+
+    auto participant = rmf_traffic::schedule::make_participant(
+          std::move(description), *database);
+
+    return RobotUpdateHandle::Implementation::make_shared(
+          *database,
+          std::move(participant),
+          planner,
+          std::move(command),
+          std::move(position));
+  }
+};
+
+//==============================================================================
+std::shared_ptr<RobotUpdateHandle> FleetUpdateHandle::add_robot(
+    std::shared_ptr<RobotCommandHandle> handle,
+    const std::string& name,
+    const rmf_traffic::Profile& profile,
+    std::size_t initial_waypoint,
+    double orientation)
+{
+  return _pimpl->add_robot(
+        handle, name, profile,
+        make_starts(initial_waypoint, orientation));
+}
+
+//==============================================================================
+std::shared_ptr<RobotUpdateHandle> FleetUpdateHandle::add_robot(
+    std::shared_ptr<RobotCommandHandle> handle,
+    const std::string& name,
+    const rmf_traffic::Profile& profile,
+    const Eigen::Vector3d& initial_position,
+    const std::vector<std::size_t>& initial_lanes)
+{
+  return _pimpl->add_robot(
+        handle, name, profile,
+        make_starts(
+          _pimpl->planner->get_configuration().graph(),
+          initial_position, initial_lanes));
+}
+
+//==============================================================================
+FleetUpdateHandle::FleetUpdateHandle()
+{
+  // Do nothing
+}
+
+//==============================================================================
+class TestScenario::Implementation
+{
+public:
+
+  std::shared_ptr<rmf_traffic::schedule::Database> database;
+
+  Implementation()
+    : database(std::make_shared<rmf_traffic::schedule::Database>())
+  {
+    // Do nothing
+  }
+
+};
+
+//==============================================================================
+FleetUpdateHandle TestScenario::add_fleet(
+    std::string fleet_name,
+    rmf_traffic::agv::Graph nav_graph,
+    rmf_traffic::agv::VehicleTraits traits)
+{
+  return FleetUpdateHandle::Implementation::make(
+        _pimpl->database,
+        std::move(fleet_name),
+        std::move(nav_graph),
+        std::move(traits));
+}
+
+//==============================================================================
+void TestScenario::test(std::vector<Condition> conditions)
+{
+  for (const auto& condition : conditions)
+  {
+    RobotUpdateHandle::Implementation::set_goal(
+          *condition.robot, condition.goal);
+  }
+}
+
+//==============================================================================
+TestScenario::TestScenario()
+  : _pimpl(rmf_utils::make_unique_impl<Implementation>())
+{
+  // Do nothing
+}
+
+} // namespace rmf_mock_adapter
